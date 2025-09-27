676c7d0436a7408197978216780250a7b69d7b22
Daniel Offei <archimedes301@gmail.com>
2025-09-27 11:28:54 +0000

    Add script for arithmetic operation.

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "Error! Cannot divide by zero."
+            return num1 / num2
+        case _:
+            print("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+
